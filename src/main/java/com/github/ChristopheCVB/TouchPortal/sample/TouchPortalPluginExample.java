3e0c3fecb394fc3034b11ac7efcb7f9d73bb8638
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@bobler.com>
2020-05-28 11:15:48 +0200

    feat: Adding Annotation Processing (WIP) core: Rename Helpers

--- src/main/java/com/github/ChristopheCVB/TouchPortal/sample/TouchPortalPluginExample.java
+++ src/main/java/com/github/ChristopheCVB/TouchPortal/sample/TouchPortalPluginExample.java
@@ -1,17 +1,18 @@
 package com.github.ChristopheCVB.TouchPortal.sample;
 
+import com.github.ChristopheCVB.TouchPortal.Annotations.Action;
 import com.github.ChristopheCVB.TouchPortal.Annotations.Data;
 import com.github.ChristopheCVB.TouchPortal.Annotations.Plugin;
 import com.github.ChristopheCVB.TouchPortal.TouchPortalPlugin;
 
 @Plugin(version = 1000, colorDark = "#203060", colorLight = "#4070F0")
 public class TouchPortalPluginExample extends TouchPortalPlugin {
-    @com.github.ChristopheCVB.TouchPortal.Annotations.Action(name = "Dummy Action")
+    @Action(name = "Dummy Action")
     private void dummyWithData(@Data String msg) {
         System.out.println("dummyWithData");
     }
 
-    @com.github.ChristopheCVB.TouchPortal.Annotations.Action()
+    @Action()
     private void dummyWithoutData(String msg) {
         System.out.println("dummyWithoutData");
     }
